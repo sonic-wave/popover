6db753093a3e5b2fb2a78d45ae9f60c7dc870d80
Ivan <eyeonex.info@gmail.com>
2024-01-13 13:40:20 +0300

    change popover.js

--- src/js/popover.js
+++ src/js/popover.js
@@ -1,16 +1,3 @@
-// export default function popoverFunc() {
-//     const btn = document.querySelector('.btn');
-//     const popover = document.querySelector('.popover');
-
-//     btn.addEventListener('click', () => {
-//         popover.classList.toggle('hidden');
-//     })
-
-//     console.log(btn);
-//     console.log(popover);
-// }
-
-// popoverFunc();
 export default function popoverFunc() {
   const popover = document.querySelector(".popover");
 
