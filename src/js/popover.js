5eea1a0473cdb68a6cc9c0c61ace2c1b9d61262e
Ivan <eyeonex.info@gmail.com>
2024-01-16 23:58:41 +0300

    change popover.js

--- src/js/popover.js
+++ src/js/popover.js
@@ -1,5 +1,37 @@
 export default function popoverFunc() {
-  const popover = document.querySelector(".popover");
+  const popoverExist = document.querySelector(".popover");
 
-  popover.classList.toggle("hidden");
+  if (popoverExist) {
+    popoverExist.classList.toggle("hidden");
+    setTimeout(() => popoverExist.remove(), 1);
+
+    return;
+  }
+  const popover = document.createElement("div");
+  popover.className = "popover";
+
+  const arrow = document.createElement("div");
+  arrow.className = "arrow";
+
+  const popoverHeader = document.createElement("h3");
+  popoverHeader.className = "popover-header";
+  popoverHeader.textContent = "Popover title";
+
+  const popoverBody = document.createElement("div");
+  popoverBody.className = "popover-body";
+  popoverBody.textContent = `And here's some amazing content. It's very engaging. Right?`;
+
+  popover.appendChild(arrow);
+  popover.appendChild(popoverHeader);
+  popover.appendChild(popoverBody);
+
+  const { left, top } = this.getBoundingClientRect();
+
+  setTimeout(() => {
+    popover.style.top = top - popover.offsetHeight - arrow.offsetHeight + "px";
+    popover.style.left =
+      left + this.offsetWidth / 2 - popover.offsetWidth / 2 + "px";
+  }, 1);
+
+  this.before(popover);
 }
